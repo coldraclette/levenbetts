1091758a4575cbe4091758a3641937fc77842fe5
Timo Tännler <timo.taennler@protonmail.com>
2024-01-18 18:30:44 +0200

    Refactor SwiperOverview component and update CSS styles

--- src/app/(site)/components/SwiperOverview.tsx
+++ src/app/(site)/components/SwiperOverview.tsx
@@ -16,47 +16,48 @@ interface SwiperOverviewProps {
 
 export default function SwiperOverview({ projects }: SwiperOverviewProps) {
   return (
-    <Swiper
-      slidesPerView="auto"
-      className="category-overview h-full w-full"
-      mousewheel={true}
-      modules={[Mousewheel]}
-      freeMode={{ enabled: true, momentumBounce: false }}
-    >
-      {projects &&
-        projects.map((project: Project) => {
-          const imageUrl = project.landingPageImage || project.projectImage;
-          return (
-            <SwiperSlide
-              key={project._id}
-              className="mr-[15px] last-of-type:mr-0"
-            >
-              <Link
-                href={`/work/${project.category.title}/${project.slug.current}`}
-              >
-                <div className="project-title mb-[14px]">
-                  <h2>{project.title}</h2>
-                  {project.subtitle && <h3>{project.subtitle}</h3>}
-                </div>
-                <div className="relative h-full w-full">
-                  <Image
-                    src={urlForImage(imageUrl, 2400)}
-                    alt={
-                      (project.landingPageImage?.alt as string) ||
-                      (project.projectImage?.alt as string)
-                    }
-                    sizes="(min-width: 1620px) 1536px, calc(95.08vw + 15px)"
-                    fill
-                    priority
-                    className="h-full w-full object-cover"
-                    placeholder="blur"
-                    blurDataURL={imageUrl.asset.metadata.lqip}
-                  />
-                </div>
-              </Link>
-            </SwiperSlide>
-          );
-        })}
-    </Swiper>
+    <div className="category-overview absolute bottom-0 left-0 w-full lg:flex">
+      <Swiper
+        slidesPerView={'auto'}
+        className="scrollbar absolute bottom-0 left-0 w-full"
+        mousewheel={true}
+        modules={[Mousewheel]}
+        spaceBetween={15}
+        freeMode={{ enabled: true, momentumBounce: false }}
+      >
+        {projects &&
+          projects.map((project: Project) => {
+            const imageUrl = project.landingPageImage || project.projectImage;
+            return (
+              <SwiperSlide key={project._id} className="w-auto">
+                <Link
+                  href={`/work/${project.category.title}/${project.slug.current}`}
+                >
+                  <div className="project-title mb-[14px]">
+                    <h2>{project.title}</h2>
+                    {project.subtitle && <h3>{project.subtitle}</h3>}
+                  </div>
+                  <div className="category-overview">
+                    <Image
+                      src={urlForImage(imageUrl, 2400)}
+                      alt={
+                        (project.landingPageImage?.alt as string) ||
+                        (project.projectImage?.alt as string)
+                      }
+                      sizes="(min-width: 1620px) 1536px, calc(95.08vw + 15px)"
+                      height={800}
+                      width={1200}
+                      priority
+                      className="h-full w-auto object-contain"
+                      placeholder="blur"
+                      blurDataURL={imageUrl.asset.metadata.lqip}
+                    />
+                  </div>
+                </Link>
+              </SwiperSlide>
+            );
+          })}
+      </Swiper>
+    </div>
   );
 }
