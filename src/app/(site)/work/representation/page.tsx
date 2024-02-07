cf900378a0d39eade57a259d6b531ff624600f02
Timo Tännler <timo.taennler@protonmail.com>
2024-02-07 18:43:14 +0200

    add revalidate

--- src/app/(site)/work/representation/page.tsx
+++ src/app/(site)/work/representation/page.tsx
@@ -2,6 +2,8 @@ import { getResearchPageData } from '../../../../../sanity/sanity.query';
 import ResearchDesktop from '../../components/representation/ResearchDesktop';
 import ResearchMobile from '../../components/representation/ResearchMobile';
 
+export const revalidate = 60;
+
 export default async function Page() {
   const data = await getResearchPageData();
 
